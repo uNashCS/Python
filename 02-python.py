1cfded791c6b08521ca7644831c7bab9575eb509
uNashCS <80352244+uNashCS@users.noreply.github.com>
2021-06-22 15:47:00 -0500

    Introducción a la programación, IDLE de python e inslalación de Python en VSC

---
+++ 02-python.py
@@ -0,0 +1,19 @@
+# >>> promt
+print("Hola mundo"); print("Hola a todos")
+mi_nombre="Cristopher Salas"
+print(mi_nombre)
+mi_nombre="Mi nombres es \
+Cristopher\
+Salas"
+print(mi_nombre)
+# Identación para dar pertenecia a alguna función
+a=0
+for i in range(5):      # Formato for: for i in range():
+    a+=1
+    print(a)
+print("######################")
+for i in range(5):
+    for j in range(5):
+        for k in range(5):
+            print(i," ",j," ",k)
+
